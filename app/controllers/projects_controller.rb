f09907df7930f9945094042fd8baf473474fb1af
eldar219@gmail.com <eldar219@gmail.com>
2015-09-24 00:41:22 +0500

    Finalize the projects page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,8 @@ class ProjectsController < ApplicationController
 
 
   def index
-    @projects = Project.all.order("created_at desc")
+
+    @projects = Project.all.order("created_at desc").paginate(page: params[:page], per_page: 3)
   end
 
   def new
