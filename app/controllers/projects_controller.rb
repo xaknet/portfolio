3db7e9b8716d78d2a348d3ae590d706a74d01ade
eldar219@gmail.com <eldar219@gmail.com>
2015-09-24 13:22:31 +0500

    Added registration functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
 
   before_action :find_project, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
 
   def index
